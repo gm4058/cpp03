b1547678f710fb31f88de2ac120141d6d0bc6ccd
gm4058 <gm4058@naver.com>
2025-10-20 14:09:58 +0900

    ClapTrap header save

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -7,13 +7,21 @@ class ClapTrap
 {
 private:
     const std::string name;
-    int hitPoint;
-    int EnergyPoint;
-    int AttackDamage;
+    unsigned int hitPoint;
+    unsigned int EnergyPoint;
+    unsigned int AttackDamage;
+
 public:
     ClapTrap();
     ClapTrap(const std::string& name);
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
 };
+
+ClapTrap::ClapTrap()
+    : name("default"), hitPoint(10), EnergyPoint(10), AttackDamage(0) {}
 
 ClapTrap::ClapTrap(const std::string& name)
     : name(name), hitPoint(10), EnergyPoint(10), AttackDamage(0) {}
